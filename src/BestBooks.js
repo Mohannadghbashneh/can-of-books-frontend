deebfb6e8b9d45610a10b749f2c0733237ae76ca
mohannadghbashneh <ghbashneh99@gmail.com>
2022-09-05 16:33:57 +0300

    lab1

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel';
+import './BestBooks.css'
+
+
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -8,22 +13,88 @@ class BestBooks extends React.Component {
     }
   }
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  componentDidMount = () =>{
+    axios
+    .get(`http://localhost:3001/Books`)
+    .then(result =>{
+    
+     
+        this.setState({
+          books:result.data
+        })
+     
+     
+     
+    })
+    .catch(err =>{
+      console.log(err);
+    })
+  }
+
 
   render() {
 
-    /* TODO: render all the books in a Carousel */
 
     return (
-      <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+      <div id = "MainDiv">
+        
 
         {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+<div id="myDiv" style={{width:"600px" }}>
+<Carousel >
+      <Carousel.Item>
+        <img 
+          className="d-block w-100"
+         
+          src="https://images-na.ssl-images-amazon.com/images/I/416WFGrlxBL._SX328_BO1,204,203,200_.jpg"
+          
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3> {this.state.books[0].title}</h3>
+        
+          <p >{this.state.books[0].description}</p>
+          
+          <h3 >{this.state.books[0].states}</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1641403203i/57945316.jpg"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+        <h3>{this.state.books[1].title}</h3>
+          <p>{this.state.books[1].description}</p>
+          <h3>{this.state.books[1].states}</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://m.media-amazon.com/images/I/41Jt+uR7gAL.jpg"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+        <h3>{this.state.books[2].title}</h3>
+          <p>{this.state.books[2].description}</p>
+          <h3>{this.state.books[2].states}</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+   
+    </Carousel>
+</div>
+
+
+
+          
         ) : (
-          <h3>No Books Found :(</h3>
+          <h3>The Books Collection Is Empty :(</h3>
         )}
-      </>
+      </div>
     )
   }
 }
